d8c852b88e911496727458a4e5a670eb16e71dc8
Jairo Esteban Martínez Portillo <jairoboss00@gmail.com>
2023-03-13 16:35:08 -0600

    first commit

---
+++ api/src/app.module.ts
@@ -0,0 +1,35 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SeedModule } from './seed/seed.module';
+import { CommonModule } from './common/common.module';
+import { LoggerMiddleware } from './utils/logger.middleware';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule,
+    SeedModule,
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
